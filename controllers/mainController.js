5b8ef04746f0b80fb8ea92c160eef6f5ef092175
reqinamarie <reg.masilang@gmail.com>
2020-09-02 09:50:26 +0800

    modified mainController.js getMain to contain temporary values for hbs files

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,7 +3,27 @@ const path = require('path');
 const mainController = {
     //Render main page
     getMain: function(req, res, next){
-        res.sendFile(path.join(__dirname + '/../views/main.html'));
+        var details = {
+            totalSold: 400,
+            daysLeft: 2,
+            hoursLeft: 11,
+            foliosGiven: 400,
+            artist: [{
+                artistID: 1,
+                artistName: "Artist 1"
+            },
+            {
+                artistID: 2,
+                artistName: "Artist 2"
+            },
+            {
+                artistID: 3,
+                artistName: "Artist 3"
+            }]
+        }
+
+        res.render('main', details);
+        // res.sendFile(path.join(__dirname + '/../views/main.html'));
     },
 
     postOrderCheckOut: function(req, res, next){
