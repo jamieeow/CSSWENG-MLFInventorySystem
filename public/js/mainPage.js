76a0bfc197e3dd3c65518f42ddcc1d920f472085
reqinamarie <reg.masilang@gmail.com>
2020-09-04 00:30:55 +0800

    checkout list updates when item cards are clicked

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,10 +1,36 @@
+var buyCart = []
+var inCart = []
+var totalPrice = 0
+
+function buyItem(itemID, itemName, itemPrice) {
+    var i = inCart.indexOf(itemID)
+
+    if (i == -1) {
+        inCart.push(itemID)
+        buyCart.push({itemID: itemID, quantity: 1})
+
+        $("#checkoutItemsList").append("<tr id='" + itemID + "Cart'><td id='" + itemID + "Quantity'>(1) " + itemName + "</td><td id='" + itemID + "Total' class='text-right'>" + parseFloat(itemPrice) + "</td></tr>")
+    } else {
+        buyCart[i].quantity += 1
+        var q = buyCart[i].quantity
+        var p = q * parseFloat(itemPrice)
+
+        $("#" + itemID + "Quantity").html("(" + q + ") " + itemName);
+        $("#" + itemID + "Total").html(parseFloat(p));
+    }
+    
+    totalPrice += parseFloat(itemPrice)
+    $("#totalPrice").html(parseFloat(totalPrice))
+    console.log(buyCart)
+}
+
 $(document).ready(function () {
     $(".itemGrid").hide()
     
     $("select[name='selectedArtist']").change(function() {
         var selected = $(this).children("option:selected").val();
         $(".itemGrid").hide()
-        $("#" + selected + "-buyItemSection").show()
+        $("#" + selected + "-buyItem").show()
     })
     
     $("select[name='financeSelectedArtist']").change(function() {
