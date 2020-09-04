c3bc3dd830fe3c55e1602590bfb48d9b0a346290
howard <howard.montecillo@yahoo.com>
2020-09-04 19:10:19 +0800

    added delete collection script

--- createDatabase.js
+++ createDatabase.js
@@ -1,23 +1,9 @@
 const db = require('./models/database.js');
-const Admins = require('./models/AdminModel.js');
-const Artists = require('./models/ArtistModel.js');
-const Bundles = require('./models/BundleModel.js');
-const Cashiers = require('./models/CashierModel.js');
-const Events = require('./models/EventModel.js');
-const Items = require('./models/ItemModel.js');
 
-db.connect();
-
-db.deleteMany(Admins, {}, callback=>{
-    db.deleteMany(Artists, {}, callback=>{
-        db.deleteMany(Bundles, {}, callback=>{
-            db.deleteMany(Cashiers, {}, callback=>{
-                db.deleteMany(Events, {}, callback=>{
-                    db.deleteMany(Items, {}, callback=>{
-                        db.close();
-                    });
-                });
-            });
-        });
-    });
-});
+db.createDatabase();
+db.createCollection("admins");
+db.createCollection("artists");
+db.createCollection("bundles");
+db.createCollection("cashiers");
+db.createCollection("events");
+db.createCollection("items");
--- deleteDatabase.js
+++
@@ -1,9 +0,0 @@
-const db = require('./models/database.js');
-
-db.createDatabase();
-db.createCollection("admins");
-db.createCollection("artists");
-db.createCollection("bundles");
-db.createCollection("cashiers");
-db.createCollection("events");
-db.createCollection("items");
