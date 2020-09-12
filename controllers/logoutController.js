fd624b00a6c24534fb9c716f403c54e63e9493bc
reqinamarie <reg.masilang@gmail.com>
2020-09-12 15:45:24 +0800

    implemented logout function

---
+++ controllers/logoutController.js
@@ -0,0 +1,14 @@
+const logoutController = {
+
+    getLogout: function (req, res) {
+        
+        req.session.destroy(function(err) {
+            if(err) throw err;
+            res.redirect('/');
+        });
+
+    }
+
+}
+
+module.exports = logoutController;
