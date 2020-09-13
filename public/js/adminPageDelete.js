4886086e3cc79c280761522c9e0f28bd458a883b
howard <howard.montecillo@yahoo.com>
2020-09-13 16:07:31 +0800

    changed some identical id variable names

--- public/js/adminPageDelete.js
+++ public/js/adminPageDelete.js
@@ -22,6 +22,79 @@ function deleteArtist(e) {
         return false;
     }
 }
+
+function deleteItem(e) {
+    var selected = $("select[name='artistsListDropdownEdit']").children("option:selected").val();
+    if (selected != '') {
+        if(!confirm('Are you sure you want to delete?')) {
+            e.preventDefault();
+            return false;
+        }
+        else {
+            $.post('/admin/deleteArtist', {artistID: selected}, function(result){
+                if (result) {
+                    console.log("post success");
+                    window.location = '/admin';
+                    return true;
+                }
+            })
+        }
+    }
+    else {
+        alert('Please select an artist!');
+        e.preventDefault();
+        return false;
+    }
+}
+
+function deleteBundle(e) {
+    var selected = $("select[name='artistsListDropdownEdit']").children("option:selected").val();
+    if (selected != '') {
+        if(!confirm('Are you sure you want to delete?')) {
+            e.preventDefault();
+            return false;
+        }
+        else {
+            $.post('/admin/deleteArtist', {artistID: selected}, function(result){
+                if (result) {
+                    console.log("post success");
+                    window.location = '/admin';
+                    return true;
+                }
+            })
+        }
+    }
+    else {
+        alert('Please select an artist!');
+        e.preventDefault();
+        return false;
+    }
+}
+
+function deleteEvent(e) {
+    var selected = $("select[name='artistsListDropdownEdit']").children("option:selected").val();
+    if (selected != '') {
+        if(!confirm('Are you sure you want to delete?')) {
+            e.preventDefault();
+            return false;
+        }
+        else {
+            $.post('/admin/deleteArtist', {artistID: selected}, function(result){
+                if (result) {
+                    console.log("post success");
+                    window.location = '/admin';
+                    return true;
+                }
+            })
+        }
+    }
+    else {
+        alert('Please select an artist!');
+        e.preventDefault();
+        return false;
+    }
+}
+
 $(document).ready(function () {
     
 });
